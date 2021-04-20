4b6d4d3e7afa7777807969fd1c4c313e8d72dcb1
Kyler England <kylerengland32@gmail.com>
2021-04-19 21:43:59 -0400

    Deleted all button files as they were completely unneccessary.

---
+++ src/jalif/jalif/indwes/myemail/edu/PaintTest.java
@@ -0,0 +1,20 @@
+package jalif.jalif.indwes.myemail.edu;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PaintTest {
+
+	@Test
+	void test1() {
+		Canvas c = new Canvas();
+		
+	}
+	
+	void test2(){
+		
+	}
+
+}
